e57f568b78d1b9e3d3adfe5f7ea5e573cfede345
seann27 <35455522+seann27@users.noreply.github.com>
2021-07-10 10:41:25 -0400

    Updated readme, added main script

--- get_NCBI_human_proteins.py
+++ get_NCBI_human_proteins.py
@@ -1,5 +1,4 @@
-# NCBI_ProteinDB_Search
-
+'''
 DESCRIPTION:
 - This script runs get requests to the Entrez API to get
 a full list of human protein accession numbers from NCBI's protein database
@@ -33,3 +32,51 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'''
+
+import requests, pickle, math, time
+import xml.etree.ElementTree as ET
+from tqdm import tqdm
+
+# globals
+baseurl = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=protein&term=human[orgn]"
+accession_nums = []
+
+def parse_ids(content):
+	ids = []
+	root = ET.fromstring(content)
+	for child in root:
+		if child.tag == 'IdList':
+			for c in child.iter():
+				if c.tag == 'Id':
+					accession_nums.append(int(c.text))
+
+def main():
+	total_count = 0
+	# batch number is the number of results per page for the get request
+	batch_num = 10000
+
+	# get total number of protein records
+	response = requests.get(baseurl)
+	root = ET.fromstring(response.content)
+	for child in root:
+	    if child.tag == 'Count':
+	        total_count = int(child.text)
+	num_loops = math.ceil(total_count/batch_num)
+
+	# grab protein accession numbers
+	for i in tqdm(range(num_loops)):
+		if i % 3 == 0:
+			# pause between requests, needs more time if batch size is substantially increased
+			time.sleep(1.5)
+		idx = batch_num * i
+		url = f"{baseurl}&retstart={idx}&retmax={batch_num}"
+		response = requests.get(url)
+		parse_ids(response.content.decode())
+
+	# write the list to pickle file
+	with open('protein_gis.pkl', 'wb') as pickle_file:
+		pickle.dump(accession_nums, pickle_file)
+
+if __name__ == '__main__':
+	main()
